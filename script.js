661b1002184878313906fcded71cf3d3bc88e87b
neerajlodhi <neerajlodhi1993@gmail.com>
2023-05-28 23:33:52 +0530

    day1 projects Expending cards

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+// console.log("pan",panels)
+panels.forEach((panel)=> {
+// console.log(panel)
+panel.addEventListener('click',() =>{
+    // console.log(123)
+    removeActiveClasses()
+    panel.classList.add('active')
+})
+
+})
+function removeActiveClasses(){
+    panels.forEach( panel=>{
+panel.classList.remove('active')
+    })
+}
